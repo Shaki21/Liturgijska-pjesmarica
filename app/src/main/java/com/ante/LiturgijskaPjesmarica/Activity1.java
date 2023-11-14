0012af223b1fd63dbde6ca4c3e3ce59946c3e787
Shaki21 <89574925+Shaki21@users.noreply.github.com>
2023-11-14 12:15:45 +0100

    Add-main-activity Add-login-activity

---
+++ app/src/main/java/com/ante/LiturgijskaPjesmarica/Activity1.java
@@ -0,0 +1,20 @@
+package com.ante.LiturgijskaPjesmarica;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+public class Activity1 extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setBackgroundDrawableResource(R.drawable.gradient_background);
+        setContentView(R.layout.activity_activity1);
+
+
+    }
+}
+////CrimsonText-SemiBold
--- app/src/main/java/com/ante/LiturgijskaPjesmarica/MainActivity.java
+++ app/src/main/java/com/ante/LiturgijskaPjesmarica/MainActivity.java
@@ -2,13 +2,29 @@ package com.ante.LiturgijskaPjesmarica;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setBackgroundDrawableResource(R.drawable.gradient_background);
         setContentView(R.layout.activity_main);
+        TextView textView1 = findViewById(R.id.textView1);
+
+        Button prijaviSe = findViewById(R.id.prijaviSe);
+        prijaviSe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Activity1.class);
+                startActivity(intent);
+            }
+        });
     }
+//CrimsonText-SemiBold
 }
